740f5180245f426e3a3da8e92c27d18aae860a00
Robin Millette <robin@millette.info>
2020-04-08 18:40:54 -0400

    Client-side js.

---
+++ main.js
@@ -0,0 +1,8 @@
+// import { neverland as $, render, html, useState } from 'https://unpkg.com/neverland?module'
+import { render, html } from "https://unpkg.com/neverland?module";
+
+render(
+  document.body,
+  html`<h1>Hi</h1>
+    <h2>There there</h2>`
+);
